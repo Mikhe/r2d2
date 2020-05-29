66a78b55466ea23e5affa44fc2667de3f97eea59
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2020-05-29 15:50:46 +0300

    : Extend input functionality

--- src/uikit/input/index.js
+++ src/uikit/input/index.js
@@ -4,19 +4,27 @@ import { createCn } from 'bem-react-classname';
 import './input.scss';
 import { Icon } from '../index';
 
-function Input({ type, icon, placeholder, className, onChange }) {
+function Input({ type, icon, placeholder, className, onChange, subplaceholder, maxLength }) {
     const cn = createCn('input');
     const handleChange = (e) => {
-        onChange(e.target.value);
+        onChange && onChange(e.target.value);
     };
 
     return (
-        <div className={cn({ icon: !!icon })}>
-            <input type={type} placeholder={placeholder} className={className} onChange={handleChange} />
+        <div className={cn({ icon: !!icon, subplaceholder: !!subplaceholder })}>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className={className}
+                maxLength={maxLength}
+                onChange={handleChange}
+            />
 
             {icon && (
                 <Icon src={icon} width={24} height={24} />
             )}
+
+            {subplaceholder && <span className={cn('subplaceholder')}>{subplaceholder}</span>}
         </div>
     );
 }
