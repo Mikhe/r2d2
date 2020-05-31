f547abfd5ad4aef8717a887ea9138c8cdd6784f7
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2020-05-31 13:28:28 +0300

    : Add click handler to icon

--- src/uikit/icon/index.js
+++ src/uikit/icon/index.js
@@ -3,13 +3,16 @@ import { createCn } from 'bem-react-classname';
 
 import './icon.scss';
 
-function Icon({ name, width, height, className, color }) {
+function Icon({ name, width, height, className, color, onClick }) {
     const cn = createCn('icon', className);
+    const handleClick = () => {
+        onClick && onClick();
+    };
 
     color = color || 'black';
 
     return (
-        <div className={cn()}>
+        <div className={cn({ clickable: !!onClick })} onClick={handleClick}>
             <div
                 style={{ width: width, height: height }}
                 className={cn('icon', { icon: name, color })}
